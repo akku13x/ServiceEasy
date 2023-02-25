d5e87b845a8b3ba151f48002b2956a124c46c35e
Akansha <whoakakansha12.com>
2023-02-25 07:07:20 +0530

    Uploaded

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer=()=>{
+    return (
+        <>
+        <footer className="bg-light text-center">
+            <p>
+               Website Made By Akansha Singh.
+            </p>
+
+        </footer>
+
+        </>
+    );
+};
+
+export default Footer;
